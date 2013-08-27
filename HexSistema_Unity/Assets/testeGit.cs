a0cfc5c65cb31800a9dec78968c27f6ae83cf3c4
marxistpokemon <enric.llagostera@gmail.com>
2013-08-27 19:15:48 -0300

    Commit de teste de Git.
    Teste, segundo passo.

---
+++ HexSistema_Unity/Assets/testeGit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testeGit : MonoBehaviour {
+	
+	public Transform camera;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
