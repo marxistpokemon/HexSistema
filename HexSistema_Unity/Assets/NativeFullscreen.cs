953fb611a164aee7bd7f65d886e2d979d1396acf
marxistpokemon <enric.llagostera@gmail.com>
2013-08-28 02:55:20 -0300

    Correcoes para build simples.
    Hexagonos e cubos funcionando como anteriormente. Agora, todos puxam de
    classes-base genéricas.

---
+++ HexSistema_Unity/Assets/NativeFullscreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NativeFullscreen : MonoBehaviour {
+	
+	void Update(){
+		if(Input.GetKeyUp(KeyCode.Space)){
+			
+			Screen.fullScreen = !Screen.fullScreen;
+			
+			if(Screen.fullScreen)
+				Screen.SetResolution (Screen.currentResolution.width, Screen.currentResolution.height, true);
+		}
+	}
+}
